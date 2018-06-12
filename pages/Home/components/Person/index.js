6af23696216356467562a9108882eea03db79ecf
Alexander Woods <alexhwoods@gmail.com>
2018-06-12 17:16:58 -0400

    Add profile page

--- pages/Home/components/Person/index.js
+++ pages/Home/components/Person/index.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Card, CardHeader, CardBody } from 'react-simple-card'
+import { Card, CardHeader, CardBody, CardFooter } from 'react-simple-card'
+import Link from 'next/link'
 
 import styles from './styles'
 import truncateChars from '../../../../services/truncateChars'
 
-const Person = ({ name, email, jobTitle }) => (
+const Person = ({ id, name, email, jobTitle }) => (
   <Card style={styles.card}>
     <CardHeader>{name}</CardHeader>
     <CardBody>
       <p>{truncateChars(email, 30)}</p>
       <p>{truncateChars(jobTitle, 30)}</p>
     </CardBody>
+    <CardFooter>
+      <Link href={{ pathname: '/profile', query: { id } }}>
+        <button>View more</button>
+      </Link>
+    </CardFooter>
   </Card>
 )
 
 Person.propTypes = {
+  id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   email: PropTypes.string.isRequired,
   jobTitle: PropTypes.string.isRequired,
--- services/network/index.js
+++ services/network/index.js
@@ -2,11 +2,11 @@ import axios from 'axios'
 import getConfig from 'next/config'
 
 // eslint-disable-next-line import/prefer-default-export
-export const getPeople = async () => {
+export const getPeople = async ids => {
   const { publicRuntimeConfig } = getConfig()
   const config = {
     method: 'get',
-    url: `${publicRuntimeConfig.apiUrl}v2/people.json`,
+    url: `${publicRuntimeConfig.apiUrl}v2/people.json${ids && `?ids=${ids}`}`,
     headers: {
       Authorization: `Bearer ${publicRuntimeConfig.apiKey}`,
       'Acess-Control-Allow-Origin': '*',
@@ -17,6 +17,7 @@ export const getPeople = async () => {
   const { data: { data } } = response
 
   return data.map(person => ({
+    id: person.id,
     name: person.display_name,
     email: person.email_address,
     jobTitle: person.title,
